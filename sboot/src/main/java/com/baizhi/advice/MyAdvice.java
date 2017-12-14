fa81f4031084aa38be029fa507c21cf064db74df
licy <licy@zparkhr.com.cn>
2017-12-14 15:30:27 +0800

    01

--- sboot/src/main/java/com/baizhi/advice/MyAdvice.java
+++ sboot/src/main/java/com/baizhi/advice/MyAdvice.java
@@ -17,10 +17,6 @@ public class MyAdvice {
 
     @Before("execution(* com.baizhi.service.*.*(..))")
     public void before(JoinPoint joinPoint) {
-        System.out.println("进入前置切面2");
-        System.out.println("进入前置切面2");
-        System.out.println("进入前置切面2");
-        System.out.println("进入前置切面2");
-        System.out.println("进入前置切面2");
+        System.out.println("进入前置切面3");
     }
 }
