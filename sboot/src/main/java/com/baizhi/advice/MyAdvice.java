22cbeba878f1490f05ac649f0bf87af31625c272
licy <licy@zparkhr.com.cn>
2017-12-14 13:43:05 +0800

    01

--- sboot/src/main/java/com/baizhi/advice/MyAdvice.java
+++ sboot/src/main/java/com/baizhi/advice/MyAdvice.java
@@ -22,8 +22,5 @@ public class MyAdvice {
         System.out.println("进入前置切面");
         System.out.println("进入前置切面");
         System.out.println("进入前置切面");
-        System.out.println("进入前置切面");
-        System.out.println("进入前置切面");
-        System.out.println("进入前置切面");
     }
 }
